634721d3e4b1b929a6935d1ae328d553d8ed9fc4
Wht Mst <ven000mus@gmail.com>
2026-09-14 16:32:24 +0400

    Add files via upload

---
+++ tapper/src-tauri/src/lib.rs
@@ -0,0 +1,8 @@
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .run(tauri::generate_context!())
+        .expect("error while running WM Tapper");
+}
